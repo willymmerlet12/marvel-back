5b347b3d88b5e4d807c01bf47048e13019d39fdc
Willy <willymmerlet@gmail.com>
2020-11-19 10:15:15 +0100

    first commit

--- routes/comics.js
+++ routes/comics.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const md5 = require("crypto-js/md5");
+const md5 = require("md5");
 const axios = require("axios");
 
 // Creation of TS
